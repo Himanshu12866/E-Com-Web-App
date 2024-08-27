c7dc281f892352bfe346615430e00120fcaebb6a
Himanshu Kumar Mishra <130586038+Himanshu12866@users.noreply.github.com>
2024-08-27 16:56:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,9 +46,7 @@ app.post("/register", async (req, res) => {
 
 // app.get("/login", async (req,res)=>{
 //     let obj = await mongoClient.connect(conStr)
-//     let database = obj.db("E-commerce")
-//     let documents = database.collection('users').findOne({})
-// })
+//     let database = obj.db("E-commerce"
 
 app.listen('5000')
 console.log("server started : http://127.0.0.1:5000")
